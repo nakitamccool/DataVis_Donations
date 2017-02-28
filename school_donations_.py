eaba88158b604f2c3d418686786f0d69d498bb00
Nakita <nakita@Nakitas-MacBook-Pro.local>
2017-02-28 11:06:39 +0000

    updated DB connections to run locally, Heroku not linking to data

--- school_donations_.py
+++ school_donations_.py
@@ -6,25 +6,25 @@ import os
 
 app = Flask(__name__)
 
-# MONGODB_HOST = 'localhost'
-# MONGODB_PORT = 27017
+MONGODB_HOST = 'localhost'
+MONGODB_PORT = 27017
 
-MONGODB_URI = os.getenv('MONGODB_URI')
+# MONGODB_URI = os.getenv('MONGODB_URI')
 DBS_NAME = 'donorsUSA'
 COLLECTION_NAME = 'projects'
 FIELDS = {'funding_status': True, 'school_state': True, 'resource_type': True, 'poverty_level': True,
           'date_posted': True, 'total_donations': True, '_id': False}
 
 
-@app.route("/")
+@app.route("/")§
 def index():
     return render_template("index.html")
 
 
 @app.route("/donorsUS/projects")
 def donor_projects():
-    # connection = MongoClient(MONGODB_HOST, MONGODB_PORT)
-    connection = MongoClient(MONGODB_URI)
+    connection = MongoClient(MONGODB_HOST, MONGODB_PORT)
+    # connection = MongoClient(MONGODB_URI)
     collection = connection[DBS_NAME][COLLECTION_NAME]
     projects = collection.find(projection=FIELDS, limit=55000)
     json_projects = []
